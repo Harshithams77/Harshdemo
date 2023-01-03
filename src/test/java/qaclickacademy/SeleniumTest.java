86a8d195efcccd83b26554df18c55fae0f52cdfb
harsh <harshitha.murali15@gmail.con>
2023-01-03 16:14:43 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from Maven
+	//how to execute all testcases from Test Folder-mvn test
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elemenetsUi");
+	}
+
+}
