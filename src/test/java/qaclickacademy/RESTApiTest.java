7711baddf5c6760786b4d61f9c09a173cde13ed5
harsh <harshitha.murali15@gmail.com>
2023-02-14 11:58:45 +0530

    American guy commit

--- src/test/java/qaclickacademy/RESTApiTest.java
+++ src/test/java/qaclickacademy/RESTApiTest.java
@@ -9,7 +9,14 @@ public class RESTApiTest {
 		System.out.println("postJIRA");
 		System.out.println("postJIRA2");
 		System.out.println("postJIRA3");
-			
+		//feere
+		//ffgh
+		//fgf
+		
+		System.out.println("postJIRA4");
+		System.out.println("postJIRA5");
+		System.out.println("postJIRA6");
+		System.out.println("postJIRA7");
 	}
 	@Test
 	public void deleteTwitter()
